687c6a077fc5bf1a3d33325582529a34fb1cacf0
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-22 17:14:56 +0300

    Giriş sayfası fonksiyonları eklendi.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,15 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function __construct()
-    {
-        parent::__construct();
-    }
+	
 	public function index()
 	{
 		$this->load->library('session');
 		$user = $this->session->USER;
-		$this->load->view('home');
-		//
+		if (isset($user)){
+			$this->load->view('home');
+		}else{
+			$this->load->view('login', array("loginError"=> ""));
+		}
 	}
 }
--- application/models/Login_Model.php
+++ application/models/Login_Model.php
@@ -5,9 +5,18 @@ class Login_Model extends CI_Model{
     {
         parent::__construct();
     }
-    public function user($username, $password){
-
-        return $this->db->where('username',$username)->get("Users")->result();
+    public function loginStatus($username, $password){
+        $query = $this->db->limit(1)->where('username',$username)->get("Users"); 
+        if($query->num_rows() > 0){
+            $pass = $query->result()[0]->password;
+            if($password == $pass){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 
 }
