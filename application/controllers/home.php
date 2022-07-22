c56967ff6626bb4b6ab131a1a150b0306151c3c2
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-22 19:11:14 +0300

    Assets eklemeleri yapıldı. Cihaz ekleme ve görüntüleme eklendi.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,15 +3,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	
+	public function __construct()
+    {
+        parent::__construct();
+		$this->load->model("Home_Model");
+    }
 	public function index()
 	{
 		$this->load->library('session');
 		$user = $this->session->USER;
 		if (isset($user)){
-			$this->load->view('home');
+			$data = array(
+				"completed_customers" => $this->Home_Model->getCompletedCustomer(),
+				"not_completed_customers" => $this->Home_Model->getNotCompletedCustomer(),
+			);
+			$this->load->view('home', $data);
 		}else{
 			$this->load->view('login', array("loginError"=> ""));
+		}
+	}
+	public function new_customer()
+	{
+		$customer_name = $this->input->post("customer_name");
+        $device = $this->input->post("device");
+        $description = $this->input->post("description");
+		$data = array(
+			"customer_name"=> $this->input->post("customer_name"),
+			"device"=> $this->input->post("device"),
+			"description"=> $this->input->post("description")
+		);
+		$insert = $this->Home_Model->addCustomer($data);
+		if($insert){
+			redirect(base_url());
+		}else{
+			redirect(base_url());
 		}
 	}
 }
--- application/views/inc/navbar.php
+++ application/views/inc/navbar.php
@@ -1,3 +1,15 @@
+<?php
+
+/*
+$activePage 
+  home
+  phones
+  pc
+  printers
+*/
+
+?>
+
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Biltek</a>
@@ -7,20 +19,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Anasayfa</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link active" aria-current="page" href="<?=base_url();?>">Anasayfa</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            AÇılır Menü
+            Cihazlar
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Test 1</a></li>
-            <li><a class="dropdown-item" href="#">Test 2</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Test 2</a></li>
+            <li><a class="dropdown-item" href="#">Tablet / Telefon</a></li>
+            <li><a class="dropdown-item" href="#">Bilgisayar</a></li>
+            <li><a class="dropdown-item" href="#">Yazıcı</a></li>
           </ul>
         </li>
       </ul>
