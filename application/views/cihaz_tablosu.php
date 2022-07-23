d02578b6f8d29bf2632007de8eda36bdcde30696
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-23 12:04:18 +0300

    Cihaz tablosu tasarımı güncellendi..

--- application/views/cihaz_tablosu.php
+++ application/views/cihaz_tablosu.php
@@ -3,48 +3,49 @@ echo '<div class="table-responsive">';
 echo '<table class="table table-bordered">
 <thead>
     <tr>
-        <th scope="col">İşlem Kodu</th>
+        <th scope="col">Cihaz Kodu</th>
         <th scope="col">Müşteri Adı</th>
         <th scope="col">Cihaz Türü</th>
         <th class="d-none d-lg-table-cell" scope="col">Cihaz</th>
+        <th>Teslim Durumu</th>
         <th class="d-none d-lg-table-cell" scope="col">Giriş Tarihi</th>
         <th scope="col" colspan="2">İşlem</th>
     </tr>
 </thead>
 <tbody id="cihazlar">';
+$teslim_durumu_1 = "Teslim Edildi";
+$teslim_durumu_0 = "Kontrol Ediliyor";
 $cihazEklendi = false;
 $sonCihazID = 0;
+$tabloOrnek = '<tr id="cihaz{id}" onClick="$(this).removeClass(\\\'success\\\')" class="{class}"><th scope="row">{id}</th><td id="{id}MusteriAdi">{musteri_adi}</td><td id="{id}CihazTuru">{cihaz_turu}</td><td id="{id}Cihaz" class="d-none d-lg-table-cell">{cihaz}</td><td id="{id}TeslimDurumu">{teslim_durumu}</td><td id="{id}Tarih" class="d-none d-lg-table-cell">{tarih}</td><td><a href="#" class="btn btn-info text-white">Görüntüle</a></td><td><a href="#"  class="btn btn-danger text-white ms-2" data-bs-toggle="modal" data-bs-target="#cihazıSilModal{id}">Sil</a></td></tr><div class="modal fade" id="cihazıSilModal{id}" tabindex="-1" aria-labelledby="cihazıSilModal{id}Label" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="cihazıSilModal{id}Label">Cihaz Silme İşlemini Onaylayın</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">Bu cihazı silmek istediğinize emin misiniz?</div><div class="modal-footer"><a href="'.base_url("anasayfa/cihazSil/").'/{id}" type="button" class="btn btn-success">Evet</a><a href="#" type="button" class="btn btn-danger" data-bs-dismiss="modal">Hayır</a></div></div></div></div>';
 foreach ($cihazlar as $cihaz) {
     if($cihazEklendi == false){
       $sonCihazID = $cihaz->id;
       $cihazEklendi = true;
     }
-    echo '<tr id="cihaz' . $cihaz->id . '">
-    <th scope="row">' . $cihaz->id . '</th>
-    <td>' . $cihaz->musteri_adi . '</td>
-    <td>' . $cihaz->cihaz_turu. '</td>
-    <td class="d-none d-lg-table-cell">' . $cihaz->cihaz . '</td>';
-    echo '</td>
-    <td class="d-none d-lg-table-cell">' . $cihaz->tarih. '</td>
-    <td><a href="#" class="btn btn-info text-white">Görüntüle</a></td>
-    <td><a href="#"  class="btn btn-danger text-white ms-2" data-bs-toggle="modal" data-bs-target="#cihazıSilModal'.$cihaz->id.'">Sil</a></td></tr>';
-    echo '<div class="modal fade" id="cihazıSilModal'.$cihaz->id.'" tabindex="-1" aria-labelledby="cihazıSilModal'.$cihaz->id.'Label" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="cihazıSilModal'.$cihaz->id.'Label">Cihaz Silme İşlemini Onaylayın</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              Bu cihazı silmek istediğinize emin misiniz?
-            </div>
-          <div class="modal-footer">
-              <a href="'.base_url("anasayfa/cihazSil/".$cihaz->id).'" type="button" class="btn btn-success">Evet</a>
-              <a href="#" type="button" class="btn btn-danger" data-bs-dismiss="modal">Hayır</a>
-          </div>
-      </div>
-    </div>
-  </div>';
+    $eskiler = array(
+      "\\",
+      "{class}",
+      "{id}",
+      "{musteri_adi}",
+      "{cihaz_turu}",
+      "{cihaz}",
+      "{teslim_durumu}",
+      "{tarih}",
+    );
+    
+    $yeniler = array(
+      "",
+      "",
+      $cihaz->id,
+      $cihaz->musteri_adi,
+      $cihaz->cihaz_turu,
+      $cihaz->cihaz,
+      $cihaz->teslim_edildi == 1 ? $teslim_durumu_1 : $teslim_durumu_0,
+      $cihaz->tarih,
+    );
+    $tablo = str_replace($eskiler, $yeniler, $tabloOrnek);
+    echo $tablo;
 }
 echo '
 </tbody>
@@ -71,6 +72,7 @@ echo '</div>';
           sonCihazID = value.id;
           sayac++;
         }
+        $("#"+value.id+"TeslimDurumu").text(value.teslim_edildi == 1 ? "<?=$teslim_durumu_1;?>" : "<?=$teslim_durumu_0;?>");
       });
       if(sayac == 0){
         $("#cihazlarUyari").show();
@@ -80,7 +82,9 @@ echo '</div>';
       $.each(JSON.parse(data), function( index, value ) {
         $("#cihazlarUyari").hide();
         $("#cihaz"+value.id).remove();
-        $("#cihazlar").prepend('<tr id="cihaz' + value.id + '><th scope="row">' + value.id + '</th><td>' + value.musteri_adi + '</td><td>' + value.cihaz_turu + '</td><td class="d-none d-lg-table-cell">' + value.cihaz + '</td></td><td class="d-none d-lg-table-cell">' + value.tarih + '</td><td><a href="#" class="btn btn-info text-white">Görüntüle</a></td><td><a href="#" class="btn btn-danger text-white ms-2" data-bs-toggle="modal" data-bs-target="#cihazıSilModal' + value.id + '">Sil</a></td></tr><div class="modal fade" id="cihazıSilModal' + value.id + '" tabindex="-1" aria-labelledby="cihazıSilModal' + value.id + 'Label" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="cihazıSilModal' + value.id + 'Label">Cihaz Silme İşlemini Onaylayın</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">Bu cihazı silmek istediğinize emin misiniz?</div><div class="modal-footer"><a href="<?php echo base_url("anasayfa/cihazSil/");?>' + value.id + '" type="button" class="btn btn-success">Evet</a><a href="#" type="button" class="btn btn-danger" data-bs-dismiss="modal">Hayır</a></div></div></div></div>');
+        let tabloOrnek = '<?=$tabloOrnek;?>';
+        var tablo = tabloOrnek.replaceAll("{class}", "success").replaceAll("{id}", value.id).replaceAll("{musteri_adi}", value.musteri_adi).replaceAll("{cihaz_turu}", value.cihaz_turu).replaceAll("{cihaz}", value.cihaz).replaceAll("{teslim_durumu}", value.teslim_edildi == 1 ? "<?=$teslim_durumu_1;?>" : "<?=$teslim_durumu_0;?>").replaceAll("{tarih}", value.tarih);
+        $("#cihazlar").prepend(tablo);
       });
     });
   }, 5000);
