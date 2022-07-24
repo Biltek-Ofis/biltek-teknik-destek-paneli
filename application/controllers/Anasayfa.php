0deb516d833ae3b86ce7c0b154b262234d812ffc
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-24 18:07:21 +0300

    Panel indirilip ihtiyaçlara göre düzenlendi.

--- application/controllers/Anasayfa.php
+++ application/controllers/Anasayfa.php
@@ -1,71 +1,23 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Anasayfa extends CI_Controller {
+class Anasayfa extends CI_Controller{
 
-	public function __construct()
+    public function __construct()
     {
         parent::__construct();
-		$this->load->model("Anasayfa_Model");
     }
-	public function index()
-	{
-		if ($this->Anasayfa_Model->kullaniciGiris()){
-			$veri = array(
-				"cihazTurleri" => $this->Anasayfa_Model->cihazTurleri(),
-			);
-			$this->load->view('anasayfa', $veri);
-		}else{
-			$this->load->view('giris', array("girisHatasi"=> ""));
-		}
-	}
-	public function cihazlarJQ($id){
-		if ($this->Anasayfa_Model->kullaniciGiris()){
-			echo json_encode($this->Anasayfa_Model->cihazlarJQ($id));
-		}
-	}
-	public function cihazlarTumuJQ(){
-		if ($this->Anasayfa_Model->kullaniciGiris()){
-			echo json_encode($this->Anasayfa_Model->cihazlarTumuJQ());
-		}
-	}
-	public function cihazEkle()
-	{
-		if ($this->Anasayfa_Model->kullaniciGiris()){
-			$veri = array(
-				"musteri_adi"=> $this->input->post("musteri_adi"),
-				"cihaz_turu"=> $this->input->post("cihaz_turu"),
-				"cihaz" => $this->input->post("cihaz"),
-				"ariza_aciklamasi"=> $this->input->post("ariza_aciklamasi")
-			);
-			$ekle = $this->Anasayfa_Model->cihazEkle($veri);
-			if($ekle){
-				redirect(base_url());
-			}else{
-				$this->load->model("Kullanicilar_Model");
-				$this->Kullanicilar_Model->girisUyari("/");
-			}
-		}else{
-			$this->load->model("Kullanicilar_Model");
-			$this->Kullanicilar_Model->girisUyari("/");
-		}
-	}
-	public function cihazSil($id)
-	{
-		if ($this->Anasayfa_Model->kullaniciGiris()){
-			$sil = $this->Anasayfa_Model->cihazSil($id);
-			if($sil){
-				redirect(base_url());
-			}else{
-				redirect(base_url());
-			}
-		}else{
-			$this->load->model("Kullanicilar_Model");
-			$this->Kullanicilar_Model->girisUyari("/");
-		}
-	}
-	public function silinenCihazlariBul()
-	{
-		echo json_encode($this->Anasayfa_Model->silinenCihazlariBul());
-	}
+    public $baslikStr = "baslik";
+    public $icerikStr = "icerik";
+    public function index(){
+        $this->load->view("tasarim", $this->tasarimArray("Anasayfa", "test_icerik"));
+    }
+    public function tasarimArray($baslik, $icerik){
+        return array(
+            "baslik"=> $baslik,
+            "icerik"=> $icerik,
+        );
+    }
+
 }
+
+?>
--- application/views/inc/styles.php
+++ application/views/inc/styles.php
@@ -1,2 +1,3 @@
-<link href="<?=base_url("assets/css/bootstrap.min.css");?>" rel="stylesheet">
-<link href="<?=base_url("assets/css/style.css");?>" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<link rel="stylesheet" href="<?=base_url("plugins/fontawesome-free/css/all.min.css");?>">
+<link rel="stylesheet" href="<?=base_url("dist/css/panel.min.css");?>">
