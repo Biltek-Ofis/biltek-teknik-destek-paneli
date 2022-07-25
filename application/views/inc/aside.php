6f0e10c5b2d2a9159e46c6ae10877ceff34e1241
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-25 11:02:59 +0300

    Menüye logo eklendi.

--- application/views/inc/aside.php
+++ application/views/inc/aside.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="<?= base_url(); ?>" class="brand-link w-100 text-center">
-        <!--<img src="<?= base_url("dist/img/logo.png"); ?>" alt="Biltek Bilgisayar Logo" class="brand-image elevation-3" style="opacity: .8">-->
-        <span class="brand-text font-weight-light">Biltek Bilgisayar</span>
+    <a href="<?= base_url(); ?>" class="brand-link">
+      <img src="<?= base_url("dist/img/favicon.ico"); ?>" style="height: 33px;" alt="Biltek Bilgisayar Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Biltek Bilgisayar</span>
     </a>
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
