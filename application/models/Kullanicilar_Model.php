f7fa2eeb98f83185202482e94a8dacf46f42f903
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-25 10:23:45 +0300

    Menüye kullanıcı ismi gösterimi eklendi.

--- application/models/Kullanicilar_Model.php
+++ application/models/Kullanicilar_Model.php
@@ -13,5 +13,23 @@ class Kullanicilar_Model extends CI_Model{
             window.location.replace("'.base_url($konum).'");
         }</script>';
     }
+    public $kullanicilarTablosu = "Kullanicilar";
+
+    public function kullaniciTablosu($id = "", $kullanici_adi = "", $isim = ""){
+        return array(
+            "id"=> $id,
+            "kullanici_adi"=> $kullanici_adi,
+            "isim"=> $isim,
+        );
+    }
+
+    public function kullaniciBilgileri(){
+        if($this->Giris_Model->kullaniciGiris()){
+            $kullanici = $this->db->where("kullanici_adi", $_SESSION["KULLANICI"])->get($this->kullanicilarTablosu)->result()[0];
+            return $this->kullaniciTablosu($kullanici->id, $kullanici->kullanici_adi, $kullanici->isim);
+        }else{
+            return $this->kullaniciTablosu();
+        }
+    }
 }
 ?>
