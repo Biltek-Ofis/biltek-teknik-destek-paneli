d76b1afd00b11e0406b1fa9419e963623cb07e7c
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-28 09:49:09 +0300

    Bazı hata mesajları düzenlendi.

--- application/controllers/cihazlar.php
+++ application/controllers/cihazlar.php
@@ -1,73 +1,82 @@
 <?php
 require_once("varsayilan_controller.php");
 
-class Cihazlar extends Varsayilan_Controller{
+class Cihazlar extends Varsayilan_Controller
+{
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model("Islemler_Model");
-    }
-    public function index($tur){
-        if ($this->Giris_Model->kullaniciGiris()){
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model("Islemler_Model");
+	}
+	public function index($tur)
+	{
+		if ($this->Giris_Model->kullaniciGiris()) {
 			$veri = array(
 				"baslik" => $this->Cihazlar_Model->cihazTuru($tur),
 				"cihazlar" => $this->Cihazlar_Model->cihazlarTekTur($tur),
 				"cihazTurleri" => $this->Cihazlar_Model->cihazTurleri(),
 				"suankiCihazTuru" => $tur,
 			);
-            $this->load->view("tasarim", $this->Islemler_Model->tasarimArray($this->Cihazlar_Model->cihazTuru($tur), "cihazlar", $veri));
-		}else{
-			$this->load->view('giris', array("girisHatasi"=> ""));
+			$this->load->view("tasarim", $this->Islemler_Model->tasarimArray($this->Cihazlar_Model->cihazTuru($tur), "cihazlar", $veri));
+		} else {
+			$this->load->view('giris', array("girisHatasi" => ""));
 		}
-    }
-    public function cihazlarJQ($tur, $id){
-        if ($this->Giris_Model->kullaniciGiris()){
-		    echo json_encode($this->Cihazlar_Model->cihazlarTekTurJQ($tur, $id));
-        }
 	}
-	public function cihazlarTumuJQ($tur){
-        if ($this->Giris_Model->kullaniciGiris()){
-		    echo json_encode($this->Cihazlar_Model->cihazlarTekTurTumuJQ($tur));
-        }
+	public function cihazlarJQ($tur, $id)
+	{
+		if ($this->Giris_Model->kullaniciGiris()) {
+			echo json_encode($this->Cihazlar_Model->cihazlarTekTurJQ($tur, $id));
+		}
+	}
+	public function cihazlarTumuJQ($tur)
+	{
+		if ($this->Giris_Model->kullaniciGiris()) {
+			echo json_encode($this->Cihazlar_Model->cihazlarTekTurTumuJQ($tur));
+		}
 	}
 	public function yapilanIslemlerJQ($id)
 	{
 		echo json_encode($this->Cihazlar_Model->yapilanIslemler($id)->result());
 	}
-	public function cihazSil($tur, $id){
-        if ($this->Giris_Model->kullaniciGiris()){
+	public function cihazSil($tur, $id)
+	{
+		if ($this->Giris_Model->kullaniciGiris()) {
 			$sil = $this->Cihazlar_Model->cihazSil($id);
-			if($sil){
-				redirect(base_url("cihazlar/".$tur));
-			}else{
-				redirect(base_url("cihazlar/".$tur));
+			if ($sil) {
+				redirect(base_url("cihazlar/" . $tur));
+			} else {
+				$this->Kullanicilar_Model->girisUyari("/cihazlar/" . $tur, "Silme işlemi gerçekleştirilemedi");
 			}
-		}else{
-			$this->Kullanicilar_Model->girisUyari("cihazlar/".$tur);
+		} else {
+			$this->Kullanicilar_Model->girisUyari("cihazlar/" . $tur);
 		}
-    }
-	public function teslimEdildi($tur, $id, $durum){
-        if ($this->Giris_Model->kullaniciGiris()){
-			$sil = $this->Cihazlar_Model->teslimEdildi($id, $durum);
-			if($sil){
-				redirect(base_url("cihazlar/".$tur));
-			}else{
-				redirect(base_url("cihazlar/".$tur));
+	}
+	public function teslimEdildi($tur, $id, $durum)
+	{
+		if ($this->Giris_Model->kullaniciGiris()) {
+			$guncellendi = $this->Cihazlar_Model->teslimEdildi($id, $durum);
+			if ($guncellendi) {
+				redirect(base_url("cihazlar/" . $tur));
+			} else {
+				$this->Kullanicilar_Model->girisUyari("/cihazlar/" . $tur, "Teslim durumu güncelleme işlemi gerçekleştirilemedi");
 			}
-		}else{
-			$this->Kullanicilar_Model->girisUyari("cihazlar/".$tur);
+		} else {
+			$this->Kullanicilar_Model->girisUyari("cihazlar/" . $tur);
 		}
-    }
-	public function yapilanIslemlerJS($id){
-		$yapilanIslemlerSatiri = $this->input->post("yapilanIslemlerSatiri");
-		$yapilanIslemlerSatiriBos = $this->input->post("yapilanIslemlerSatiriBos");
-		$yapilanIslemToplam = $this->input->post("yapilanIslemToplam");
-        $this->load->view("icerikler/yapilan_islemler_js",array(
-			"id"=>$id,
-			"yapilanIslemlerSatiri"=>$yapilanIslemlerSatiri,
-			"yapilanIslemlerSatiriBos"=>$yapilanIslemlerSatiriBos,
-			"yapilanIslemToplam"=>$yapilanIslemToplam,
-		));
-    }
+	}
+	public function yapilanIslemlerJS($id)
+	{
+		if ($this->Giris_Model->kullaniciGiris()) {
+			$yapilanIslemlerSatiri = $this->input->post("yapilanIslemlerSatiri");
+			$yapilanIslemlerSatiriBos = $this->input->post("yapilanIslemlerSatiriBos");
+			$yapilanIslemToplam = $this->input->post("yapilanIslemToplam");
+			$this->load->view("icerikler/yapilan_islemler_js", array(
+				"id" => $id,
+				"yapilanIslemlerSatiri" => $yapilanIslemlerSatiri,
+				"yapilanIslemlerSatiriBos" => $yapilanIslemlerSatiriBos,
+				"yapilanIslemToplam" => $yapilanIslemToplam,
+			));
+		}
+	}
 }
