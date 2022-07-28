69235888045070214f9d7c37ebcb931071073b2d
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-28 15:55:58 +0300

    Giriş sayfası düzenlendi.

--- application/views/giris.php
+++ application/views/giris.php
@@ -1,41 +1,68 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>Giriş</title>
-	<?php $this->load->view("inc/styles");?>
-    <?php $this->load->view("inc/scripts");?>
-    <link rel="stylesheet" href="<?=base_url("dist/css/giris.css");?>">
+    <meta charset="utf-8">
+    <title>Giriş</title>
+    <?php $this->load->view("inc/styles"); ?>
+    <link rel="stylesheet" href="<?= base_url("plugins/icheck-bootstrap/icheck-bootstrap.min.css"); ?>">
+    <?php $this->load->view("inc/scripts"); ?>
+    <link rel="stylesheet" href="<?= base_url("dist/css/giris.css"); ?>">
 </head>
-<body>
-
-<div id="container">
-	
-<div class="wrapper">
-        <!--<div class="logo">
-            <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="">
-        </div>-->
-        <div class="text-center mt-4 name">
-            Giriş Yap
+<body class="login-page" style="min-height: 466px;">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="<?=base_url();?>" class="h1 w-100 text-center"><img src="<?=base_url("dist/img/logo.png");?>"/></a>
+            </div>
+            <div class="card-body">
+                <div class="alert alert-danger" style="<?php if (strlen($girisHatasi) == 0) {
+                                                            echo "display:none;";
+                                                        } ?>" role="alert">
+                    <?= $girisHatasi; ?>
+                </div>
+                <form action="<?= base_url("giris"); ?>" method="post">
+                    <div class="input-group mb-3">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                        <input type="text" name="kullanici_adi" class="form-control" placeholder="Kullanıcı Adı">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                        <input type="password" name="sifre" class="form-control" placeholder="Şifre">
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Beni hatırla
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Giriş Yap</button>
+                        </div>
+                    </div>
+                </form>
+                <?php
+                //<p class="mb-1">
+                //    <a href="forgot-password.html">Şifremi Unuttum</a>
+                //</p>
+                ?>
+            </div>
         </div>
-        <form class="p-3 mt-3" method="post" action="<?= base_url("giris");?>">
-            <div class="alert alert-danger" style="<?php if(strlen($girisHatasi) == 0){ echo "display:none;";} ?>" role="alert">
-            <?=$girisHatasi;?>
-            </div>
-            <div class="form-field d-flex align-items-center">
-                <span class="far fa-user"></span>
-                <input type="text" name="kullanici_adi" id="kullanici_adi" placeholder="Kullanıcı Adı" required>
-            </div>
-            <div class="form-field d-flex align-items-center">
-                <span class="fas fa-key"></span>
-                <input type="password" name="sifre" id="sifre" placeholder="Şifre" required>
-            </div>
-            <button class="btn mt-3">Giriş Yap</button>
-        </form>
     </div>
-
-</div>
 </body>
 </html>
