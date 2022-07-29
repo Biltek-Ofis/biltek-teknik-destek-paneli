517423574cbd13363f3a6a4146c6434fbcc5cfbb
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-29 12:56:35 +0300

    Birkaç değişiklik yapıldı.

--- application/views/ogeler/aksesuar_select.php
+++ application/views/ogeler/aksesuar_select.php
@@ -1,7 +1,7 @@
 <div class="row mt-2">
     <div class="form-group col">
         <select class="form-control" name="<?= $id; ?>" aria-label="<?= $isim; ?> Hasar Durumu">
-            <option value="0" selected><?= $isim; ?> Hasar Durumu</option>
+            <option value="0" selected><?= $isim; ?> (Varsa) Hasar Durumu</option>
             <option value="1"><?= $this->Islemler_Model->hasarDurumu(1); ?></option>
             <option value="2"><?= $this->Islemler_Model->hasarDurumu(2); ?></option>
         </select>
