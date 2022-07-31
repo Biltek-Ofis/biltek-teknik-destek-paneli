74be63c3bb0947860cbe1d7ab98407382bfc01dc
Ozay Akcan <mail@ozayakcan.com.tr>
2022-07-31 23:22:03 +0300

    Cihaz düzenleme fonksiyonu eklendi.

--- application/controllers/cihaz.php
+++ application/controllers/cihaz.php
@@ -14,7 +14,7 @@ class Cihaz extends Varsayilan_Controller
             $cihaz = $this->Cihazlar_Model->cihazBul($id);
             if ($cihaz->num_rows() > 0) {
                 $cihaz_bilg =  $this->Cihazlar_Model->cihazVerileriniDonustur($cihaz->result())[0];
-                $baslik = 'Cihaz '.$cihaz_bilg->id.' Detayları';
+                $baslik = 'Cihaz ' . $cihaz_bilg->id . ' Detayları';
                 $this->load->view("tasarim", $this->Islemler_Model->tasarimArray($baslik, "cihaz", array("cihaz" => $cihaz_bilg, "baslik" => $baslik)));
             } else {
                 redirect(base_url());
@@ -24,13 +24,28 @@ class Cihaz extends Varsayilan_Controller
         }
     }
 
+    public function duzenle($id)
+    {
+        if ($this->Giris_Model->kullaniciGiris()) {
+            $veri = $this->Cihazlar_Model->cihazPost(true);
+            $duzenle = $this->Cihazlar_Model->cihazDuzenle($id, $veri);
+            if ($duzenle) {
+                //$id = $this->db->insert_id();
+                redirect(base_url("cihaz/" . $id));
+            } else {
+                $this->Kullanicilar_Model->girisUyari("", "Düzenleme işlemi gerçekleştirilemedi. ");
+            }
+        } else {
+            $this->Kullanicilar_Model->girisUyari("cikis");
+        }
+    }
     public function teknik_servis_formu($id)
     {
         $cihaz = $this->Cihazlar_Model->cihazBul($id);
         if ($cihaz->num_rows() > 0) {
             $cihaz_bilg = $cihaz->result();
             $veriler =  $this->Cihazlar_Model->cihazVerileriniDonustur($cihaz_bilg)[0];
-            $this->load->view("icerikler/teknik_servis_formu_yazdir", array("cihaz" =>$veriler));
+            $this->load->view("icerikler/teknik_servis_formu_yazdir", array("cihaz" => $veriler));
         } else {
             redirect(base_url());
         }
