df9c16e48ff48dc4ecad3f3b65c93135c99c22a4
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-01 13:23:49 +0300

    Inputların autocomplete değerleri düzenlendi.

--- application/views/ogeler/musteri_adi.php
+++ application/views/ogeler/musteri_adi.php
@@ -1,3 +1,3 @@
 <div class="form-group<?php if(isset($sifirla)) {echo " p-0 m-0";} ?> col">
-    <input id="musteri_adi" class="form-control" type="text" name="musteri_adi" placeholder="Müşteri Adı Soyadı *" value="<?php if(isset($musteri_adi_value)) {echo $musteri_adi_value;} ?>" required>
+    <input id="musteri_adi" autocomplete="one-time-code" class="form-control" type="text" name="musteri_adi" placeholder="Müşteri Adı Soyadı *" value="<?php if(isset($musteri_adi_value)) {echo $musteri_adi_value;} ?>" required>
 </div>
