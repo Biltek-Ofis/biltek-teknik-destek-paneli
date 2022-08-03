da85de737defe28830b09f0f97f6fb6c43b41a39
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-03 15:16:59 +0300

    Stok verileri artık firma veritabanından çekiliyor.

--- application/controllers/js.php
+++ application/controllers/js.php
@@ -13,7 +13,11 @@ class Js extends Varsayilan_Controller
         redirect(base_url());
     }
     public $musteriAdi = "CARI_ISIM";
+    public $stokAdi = "STOK_ADI";
     public function musteri_adi($ara){
         echo json_encode($this->Firma_Model->musteri_bilgileri($this->musteriAdi,$ara));
+    }
+    public function stok($ara){
+        echo json_encode($this->Firma_Model->stok($this->stokAdi,$ara));
     }
 }
