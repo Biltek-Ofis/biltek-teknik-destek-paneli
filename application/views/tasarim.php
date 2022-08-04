633f8ba5096f2ee7f7b92f29d165cb9b5a949aa9
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-04 11:53:00 +0300

    Başlıklar düzenlendi.

--- application/views/tasarim.php
+++ application/views/tasarim.php
@@ -1,36 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <?php $this->load->view("inc/meta");?>
-  
-  <title><?=$baslik;?></title>
+  <?php $this->load->view("inc/meta"); ?>
+
+  <title><?= $baslik; ?><?= SITE_BASLIGI != NULL ? " - " . SITE_BASLIGI : ""; ?></title>
   <?php
-  if($ek_css != ""){
+  if ($ek_css != "") {
     $this->load->view($ek_css);
-  }?>
-  <?php $this->load->view("inc/styles");?>
-  <?php $this->load->view("inc/scripts");?>
+  } ?>
+  <?php $this->load->view("inc/styles"); ?>
+  <?php $this->load->view("inc/scripts"); ?>
 
 </head>
+
 <body class="sidebar-mini layout-fixed">
   <script>
-    $(document).ready(function(){
-      setInterval(function(){
+    $(document).ready(function() {
+      setInterval(function() {
         window.location.reload();
       }, 30 * 60 * 1000);
     });
   </script>
-<div class="wrapper">
-  <?php $this->load->view("inc/navbar");?>
-  
-  <?php 
-  $this->load->view("inc/aside", array("aktifSayfa"=>$icerik,"baslik"=>$baslik, "cihazTurleri"=> $this->Cihazlar_Model->cihazTurleri()));
-  ?>
+  <div class="wrapper">
+    <?php $this->load->view("inc/navbar"); ?>
 
-  <?php $this->load->view("icerikler/".$icerik, $icerik_array);?>
+    <?php
+    $this->load->view("inc/aside", array("aktifSayfa" => $icerik, "baslik" => $baslik, "cihazTurleri" => $this->Cihazlar_Model->cihazTurleri()));
+    ?>
 
-  <?php //$this->load->view("inc/footer");?>
-</div>
+    <?php $this->load->view("icerikler/" . $icerik, $icerik_array); ?>
+
+    <?php //$this->load->view("inc/footer");
+    ?>
+  </div>
 
 </body>
+
 </html>
