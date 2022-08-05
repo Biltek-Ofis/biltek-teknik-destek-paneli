1b13768ce0b25faedebf870ba1ec8cb21ecf7add
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-05 16:44:01 +0300

    Arama parametreleri değiştirildi.

--- application/models/Firma_Model.php
+++ application/models/Firma_Model.php
@@ -12,10 +12,18 @@ class Firma_Model extends CI_Model
     }
     public $musteriTablosu = "TBLCASABIT";
     public $stokTablosu = "TBLSTSABIT";
-    public function musteri_bilgileri($aranacak,$ara){
-        return $this->firmadb()->like($aranacak,$ara)->get($this->musteriTablosu)->result();
+    public function musteri_bilgileri($aranacak, $ara)
+    {
+        return $this->ara($this->musteriTablosu, $aranacak, $ara);
+        //return $this->firmadb()->like('LOWER(' . $aranacak . ')', strtolower($ara))->get($this->musteriTablosu)->result();
     }
-    public function stok($aranacak,$ara){
-        return $this->firmadb()->like($aranacak,$ara)->get($this->stokTablosu)->result();
+    public function stok($aranacak, $ara)
+    {
+        return $this->ara($this->stokTablosu, $aranacak, $ara);
+        //return $this->firmadb()->like($aranacak, "N'" . $ara . "'")->get($this->stokTablosu)->result();
+    }
+    public function ara($tablo, $aranacak, $ara)
+    {
+        return $this->firmadb()->query("SELECT * FROM " . $tablo . " WHERE " . $aranacak . " LIKE N'%" . $ara . "%' collate Turkish_CI_AS")->result();
     }
 }
