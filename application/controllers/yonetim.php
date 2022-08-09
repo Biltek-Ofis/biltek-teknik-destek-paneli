09fc146046fe62189584e56ea197922fae064124
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-09 15:28:25 +0300

    Kullanıcılar sayfası klasor dizilimi duzenlendi.

--- application/controllers/yonetim.php
+++ application/controllers/yonetim.php
@@ -1,7 +1,7 @@
 <?php
 require_once("varsayilan_controller.php");
 
-class Kullanicilar extends Varsayilan_Controller
+class Yonetim extends Varsayilan_Controller
 {
 
 	public function __construct()
@@ -10,13 +10,17 @@ class Kullanicilar extends Varsayilan_Controller
 	}
 	public function index()
 	{
+		redirect(base_url());
+	}
+	public function kullanicilar()
+	{
 		if ($this->Kullanicilar_Model->yonetici()) {
-			$this->load->view("tasarim", $this->Islemler_Model->tasarimArray("Kullanıcılar", "kullanicilar", [], "inc/datatables"));
+			$this->load->view("tasarim", $this->Islemler_Model->tasarimArray("Kullanıcılar", "yonetim/kullanicilar", [], "inc/datatables"));
 		} else {
 			$this->Kullanicilar_Model->girisUyari();
 		}
 	}
-	public function ekle()
+	public function kullaniciEkle()
 	{
 		if ($this->Kullanicilar_Model->yonetici()) {
 			$veri = $this->Kullanicilar_Model->kullaniciPost(true);
@@ -24,41 +28,41 @@ class Kullanicilar extends Varsayilan_Controller
 			$veri["sifre"] = $this->Islemler_Model->sifrele($sifre);
 			$ekle = $this->Kullanicilar_Model->ekle($veri);
 			if ($ekle) {
-				redirect(base_url("kullanicilar"));
+				redirect(base_url("yonetim/kullanicilar"));
 			} else {
-				$this->Kullanicilar_Model->girisUyari("kullanicilar", "Kullanıcı eklenemedi lütfen daha sonra tekrar deneyin");
+				$this->Kullanicilar_Model->girisUyari("yonetim/kullanicilar", "Kullanıcı eklenemedi lütfen daha sonra tekrar deneyin");
 			}
 		} else {
 			$this->Kullanicilar_Model->girisUyari();
 		}
 	}
-	public function duzenle($id)
+	public function kullaniciDuzenle($id)
 	{
 		if ($this->Kullanicilar_Model->yonetici()) {
 			$veri = $this->Kullanicilar_Model->kullaniciPost(true);
 			$kullanici = $this->Kullanicilar_Model->kullaniciListesi($id)[0];
-			if($kullanici->sifre != $veri["sifre"]){
+			if ($kullanici->sifre != $veri["sifre"]) {
 				$sifre = $veri["sifre"];
-				$veri["sifre"] =$this->Islemler_Model->sifrele($sifre);
+				$veri["sifre"] = $this->Islemler_Model->sifrele($sifre);
 			}
 			$duzenle = $this->Kullanicilar_Model->duzenle($id, $veri);
 			if ($duzenle) {
-				redirect(base_url("kullanicilar"));
+				redirect(base_url("yonetim/kullanicilar"));
 			} else {
-				$this->Kullanicilar_Model->girisUyari("kullanicilar", "Kullanıcı düzenlenemedi lütfen daha sonra tekrar deneyin");
+				$this->Kullanicilar_Model->girisUyari("yonetim/kullanicilar", "Kullanıcı düzenlenemedi lütfen daha sonra tekrar deneyin");
 			}
 		} else {
 			$this->Kullanicilar_Model->girisUyari();
 		}
 	}
-	public function sil($id)
+	public function kullaniciSil($id)
 	{
 		if ($this->Kullanicilar_Model->yonetici()) {
 			$sil = $this->Kullanicilar_Model->sil($id);
 			if ($sil) {
-				redirect(base_url("kullanicilar"));
+				redirect(base_url("yonetim/kullanicilar"));
 			} else {
-				$this->Kullanicilar_Model->girisUyari("kullanicilar", "Kullanıcı silinemedi lütfen daha sonra tekrar deneyin");
+				$this->Kullanicilar_Model->girisUyari("yonetim/kullanicilar", "Kullanıcı silinemedi lütfen daha sonra tekrar deneyin");
 			}
 		} else {
 			$this->Kullanicilar_Model->girisUyari();
