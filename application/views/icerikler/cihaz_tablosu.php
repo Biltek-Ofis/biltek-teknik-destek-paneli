719be6ae40da445414f85e4bad9610335e9c9e65
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-15 13:17:12 +0300

    Tablo renkleri düzenlendi.

--- application/views/icerikler/cihaz_tablosu.php
+++ application/views/icerikler/cihaz_tablosu.php
@@ -18,6 +18,36 @@
   .modal.modal-fullscreen .modal-body {
     overflow-y: auto;
   }
+
+  :root {
+    --transparan: 0.3;
+    --yazi-rengi: #1f2d3d;
+  }
+
+  .bg-warning {
+    background-color: rgba(255, 193, 7, var(--transparan)) !important;
+    color: var(--yazi-rengi) !important;
+  }
+
+  .bg-pink {
+    background-color: rgba(232, 62, 140, var(--transparan)) !important;
+    color: var(--yazi-rengi) !important;
+  }
+
+  .bg-primary {
+    background-color: rgba(0, 123, 255, var(--transparan)) !important;
+    color: var(--yazi-rengi) !important;
+  }
+
+  .bg-danger {
+    background-color: rgba(220, 53, 69, var(--transparan)) !important;
+    color: var(--yazi-rengi) !important;
+  }
+
+  .bg-success {
+    background-color: rgba(40, 167, 69, var(--transparan)) !important;
+    color: var(--yazi-rengi) !important;
+  }
 </style>
 <script>
   function medyalariYukle(id) {
