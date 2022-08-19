0c5be31c6d737292ff124ae3644d70c14cac92bd
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-19 14:51:42 +0300

    Teslim alınanların zorunluluğu kaldırıldı.

--- application/views/ogeler/teslim_alinanlar.php
+++ application/views/ogeler/teslim_alinanlar.php
@@ -4,7 +4,7 @@ if (isset($sifirla)) {
     echo " p-0 m-0";
 }
 echo ' col">
-    <textarea id="teslim_alinanlar" autocomplete="off" name="teslim_alinanlar" class="form-control" rows="3" placeholder="Teslim Alınanlar" required>';
+    <textarea id="teslim_alinanlar" autocomplete="off" name="teslim_alinanlar" class="form-control" rows="3" placeholder="Teslim Alınanlar">';
 if (isset($teslim_alinanlar_value)) {
     echo $teslim_alinanlar_value;
 }
