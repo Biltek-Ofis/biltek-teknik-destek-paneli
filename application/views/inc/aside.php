58c0241012a1cb19b523d8dc38a227d8ed712b8b
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-22 13:51:49 +0300

    Bazı site ayarları .env dosyasına aktarıldı.

--- application/views/inc/aside.php
+++ application/views/inc/aside.php
@@ -10,8 +10,8 @@ echo '<style>
 </style>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="' . base_url() . '" class="brand-link">
-        <img src="' . base_url("dist/img/favicon.ico") . '" style="height: 33px;" alt="Biltek Bilgisayar Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Biltek Bilgisayar</span>
+        <img src="' . base_url("dist/img/favicon.ico") . '" style="height: 33px;" alt="' . SITE_BASLIGI . ' Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">' . SITE_BASLIGI . '</span>
     </a>
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
