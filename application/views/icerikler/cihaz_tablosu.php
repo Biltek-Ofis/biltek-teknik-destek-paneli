a5192b8c3ea24bf5dc000e015b5a4746893ba075
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-23 11:35:49 +0300

    Cihaz tablosu stili başka dosyaya aktarıldı.

--- application/views/icerikler/cihaz_tablosu.php
+++ application/views/icerikler/cihaz_tablosu.php
@@ -18,39 +18,10 @@ echo '<style>
   .modal.modal-fullscreen .modal-body {
     overflow-y: auto;
   }
-
-  :root {
-    --transparan: 0.3;
-    --transparan-kirmizi: 0.4;
-    --yazi-rengi: #1f2d3d;
-  }
-
-  .bg-warning {
-    background-color: rgba(255, 193, 7, var(--transparan)) !important;
-    color: var(--yazi-rengi) !important;
-  }
-
-  .bg-pink {
-    background-color: rgba(232, 62, 140, var(--transparan-kirmizi)) !important;
-    color: var(--yazi-rengi) !important;
-  }
-
-  .bg-primary {
-    background-color: rgba(0, 123, 255, var(--transparan)) !important;
-    color: var(--yazi-rengi) !important;
-  }
-
-  .bg-danger {
-    background-color: rgba(220, 53, 69, var(--transparan-kirmizi)) !important;
-    color: var(--yazi-rengi) !important;
-  }
-
-  .bg-success {
-    background-color: rgba(40, 167, 69, var(--transparan)) !important;
-    color: var(--yazi-rengi) !important;
-  }
 </style>
-<script>
+';
+$this->load->view("inc/style_tablo");
+echo '<script>
   function medyalariYukle(id) {
     $.post("' . base_url("medyalar") . '/" + id, {}, function(data) {
       $("#list-medyalar-" + id).html(data);
