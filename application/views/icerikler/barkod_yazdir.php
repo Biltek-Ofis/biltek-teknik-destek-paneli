9de5fab8b35bfd2bad46d307e9ac71dd6024860f
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-23 11:05:49 +0300

    Barkodda birkaç düzenleme yapıldı.

--- application/views/icerikler/barkod_yazdir.php
+++ application/views/icerikler/barkod_yazdir.php
@@ -17,18 +17,24 @@ $style = "width: 40mm;
             word-break: break-word;
             page-break-before: always;
             display: inline;";
-$barkodYukseklik = "12mm";
+$barkodYukseklik = "14mm";
 $tableStyle = "
+            table thead,table thead tr,table thead tr td{
+                height:0 !important;
+                line-height:0 !important;
+                padding:0 !important;
+                margin:0 !important;
+            }
             table tr .icerik{
                 font-size:12px !important;
                 max-height: 4mm !important;
+                display:inline;
             }
-            table tr:first-child .icerik{
-                font-size:20px !important;
-                max-height: 6mm !important;
-            }
-            table tr:nth-child(3) .icerik{
+            table tr:nth-child(2) .icerik{
                 max-height: " . $barkodYukseklik . " !important;
+            }
+            table tr:last-child .icerik, table tr:first-child .icerik{
+                font-size:10px !important;
             }
             ";
 echo '
@@ -63,36 +69,48 @@ echo '<body onafterprint="self.close()">
     $(document).ready(function(){
         JsBarcode("#barkod", "' . $cihaz->servis_no . '", {
             width: 2,
-            height: 27,
-            displayValue: false,
+            height: 30,
+            displayValue: true,
+            fontSize: 12,
+            fontOptions: "bold"
         });
         $("#barkod").css({"height":"' . $barkodYukseklik . '"});
     });
     </script>
     <table class="table table-borderless">
         <thead>
-            <tr></tr>
-            <tr></tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
         </thead>
         <tbody class="p-1">
+        <tr>
+            <td class="p-0 pl-1 pr-1 m-0 text-left" colspan="5"><div class="icerik">' . SITE_BASLIGI . '</div></td>
+            <td class="p-0 pl-1 pr-1 m-0 text-right" colspan="7"><div class="icerik">' . substr($cihaz->tarih, 0, -5) . '</div></td>
+        </tr>
+        <tr>
+            <td class="p-0 pl-1 pr-1 m-0 text-left" colspan="12"><div class="icerik"><svg style="width:100%;" id="barkod"></svg></div></td>
+        </tr>
             <tr>
-                <td class="p-0 pl-1 pr-1 m-0 text-left" colspan="2"><div class="icerik">' . $cihaz->servis_no . '</div></td>
-            </tr>
-            <tr>
-                <td class="p-0 pl-1 pr-1 m-0" colspan="2"><div class="icerik">';
-if (strlen($cihaz->musteri_adi) > 38) {
-    // echo explode(";;", wordwrap($cihaz->musteri_adi, 30, ";;", true))[0] . '...';
-    echo rtrim(str_split($cihaz->musteri_adi, 35)[0]) . '...';
+                <td class="p-0 pl-1 pr-1 m-0 text-left" colspan="12"><div class="icerik">';
+if (strlen($cihaz->musteri_adi) > 40) {
+    // echo rtrim(explode(";;", wordwrap($cihaz->musteri_adi, 35, ";;"))[0]) . '...';
+    echo rtrim(str_split($cihaz->musteri_adi, 40)[0]) . '...';
 } else {
     echo $cihaz->musteri_adi;
 }
 echo '</div></td>
-            </tr>
-            <tr>
-                <td class="p-0 pl-1 pr-1 m-0 text-left" colspan="2"><div class="icerik"><svg id="barkod"></svg></div></td>
-            </tr>
-            <tr>
-                <td class="p-0 pl-1 pr-1 m-0 text-right" colspan="2"><div class="icerik">' . $cihaz->tarih . '</div></td>
             </tr>
         </tbody>
     </table>
