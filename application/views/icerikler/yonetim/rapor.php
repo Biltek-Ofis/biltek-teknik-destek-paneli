bf6faa9f8515205129dca6adf67e2c6f8c5a4d1a
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-25 14:02:54 +0300

    Raporda sayfa özellikleri düzenlendi. Bölüm kartları ayrıldı.

--- application/views/icerikler/yonetim/rapor.php
+++ application/views/icerikler/yonetim/rapor.php
@@ -31,6 +31,12 @@ echo '<div class="content-wrapper">
             var girisTarihiInput1 = "#giris_tarihi_baslangic_ara";
             var girisTarihiInput2 = "#giris_tarihi_bitis_ara";
             var girisTarBas, girisTarBit;
+            var sayfaSayisiUstInput = "#sayfa_sayisi_ust";
+            var sayfaSayisiUstGoster = false;
+            var sayfaSayisiAltInput = "#sayfa_sayisi_alt";
+            var sayfaSayisiAltGoster = false;
+            var sayfaSayisiSelect = "#sayfaSayisiKonumu";
+            var sayfaSayisiKonumu = "1";
             function filtreText(input, sira){
                 $.fn.dataTable.ext.search.push(
                     function( settings, searchData, index, rowData, counter ) {
@@ -107,24 +113,104 @@ echo '<div class="content-wrapper">
                     }
                 );
             }
+            function baslik(){
+                var bugun = new Date();
+                bugun.setDate(bugun.getDate() + 20);
+                return "' . SITE_BASLIGI . ' Teknik Servis Raporu - " + bugun.getFullYear() + "/"
+                + ("0" + bugun.getMonth()).slice(-2) + "/"
+                + ("0" + bugun.getDate()).slice(-2) + " " + ("0" + (bugun.getHours())).slice(-2) + ":" + ("0" + (bugun.getMinutes())).slice(-2);
+            }
             $(document).ready(function() {
+                var customize = function ( doc ) {
+                    if(sayfaSayisiUstGoster){
+                        doc["header"] = (function(page, pages) {
+                            return {
+                                columns: [
+                                    sayfaSayisiKonumu == "3" ? {
+                                        alignment: "left",
+                                        text: [
+                                            { text: page.toString(), italics: true },
+                                        ]
+                                    } : "",
+                                    sayfaSayisiKonumu == "2" ? {
+                                        alignment: "center",
+                                        text: [
+                                            { text: page.toString(), italics: true },
+                                        ]
+                                    } : "",
+                                    sayfaSayisiKonumu == "1" ? {
+                                        alignment: "right",
+                                        text: [
+                                            { text: page.toString(), italics: true },
+                                        ]
+                                    } : ""
+                                ],
+                                margin: [10, 10],
+                                fontSize:16
+                            }
+                        });
+                    }
+                    
+                    if(sayfaSayisiAltGoster){
+                        doc["footer"] = (function(page, pages) {
+                            return {
+                                columns: [
+                                    sayfaSayisiKonumu == "3" ? {
+                                        alignment: "left",
+                                        text: [
+                                            { text: page.toString(), italics: true },
+                                        ]
+                                    } : "",
+                                    sayfaSayisiKonumu == "2" ? {
+                                        alignment: "center",
+                                        text: [
+                                            { text: page.toString(), italics: true },
+                                        ]
+                                    } : "",
+                                    sayfaSayisiKonumu == "1" ? {
+                                        alignment: "right",
+                                        text: [
+                                            { text: page.toString(), italics: true },
+                                        ]
+                                    } : ""
+                                ],
+                                margin: [10, 10],
+                                fontSize:16
+                            }
+                        });
+                    }
+                    //doc.defaultStyle.fontSize = 16;
+                }
                 var cihazlarTablosu = $("#" + tabloDiv).DataTable(' . $this->Islemler_Model->datatablesAyarlari([0, "desc"], 'true', '
                 "buttons": [{
                     extend: "copyHtml5",
-                    pageSize: "LEGAL"
+                    title: baslik(),
+                    pageSize: "LEGAL",
+                    title: baslik(),
+                    customize: customize,
                 }, {
                     extend: "csv",
-                    pageSize: "LEGAL"
+                    title: baslik(),
+                    pageSize: "LEGAL",
+                    title: baslik(),
+                    customize: customize,
                 }, {
                     extend: "excel",
-                    pageSize: "LEGAL"
+                    title: baslik(),
+                    pageSize: "LEGAL",
+                    title: baslik(),
+                    customize: customize,
                 }, {
                     extend: "pdfHtml5",
                     orientation: "landscape",
-                    pageSize: "LEGAL"
+                    pageSize: "LEGAL",
+                    title: baslik(),
+                    customize: customize,
                 }, {
                     extend: "print",
-                    pageSize: "LEGAL"
+                    pageSize: "LEGAL",
+                    title: baslik(),
+                    customize: customize,
                 }],', 'setTimeout(function(){cihazlarTablosu.buttons().container().appendTo("#" + tabloDiv + "_wrapper .col-md-6:eq(0)");}, 10);') . ');
                 filtreStartsWith(yilInput, 0)
                 filtreText(musteriInput,1);
@@ -143,18 +229,40 @@ echo '<div class="content-wrapper">
                 $(musteriInput + ", " + cihazMarkaInput + ", " + cihazModelInput + ", " + girisTarihiInput1 + ", " + girisTarihiInput2 + ", " + yilInput + ", " + cihazTuruInput).on("keyup change", function(){
                     cihazlarTablosu.draw();
                 });
+                $(sayfaSayisiUstInput).change(function() {
+                    if(this.checked) {
+                        sayfaSayisiUstGoster = true;
+                    }else{
+                        sayfaSayisiUstGoster = false;
+                    } 
+                });
+                $(sayfaSayisiAltInput).change(function() {
+                    if(this.checked) {
+                        sayfaSayisiAltGoster = true;
+                    }else{
+                        sayfaSayisiAltGoster = false;
+                    } 
+                });
+                $(sayfaSayisiSelect).change(function() {
+                    sayfaSayisiKonumu = $(this).val();
+                });
             });
             </script>
                 <table class="table table-borderless mb-3">
                     <thead>
                         <tr>
-                            <th></th>
-                            <th></th>
+                            <th style="width:calc(100% / 6)"></th>
+                            <th style="width:calc(100% / 6)"></th>
+                            <th style="width:calc(100% / 6)"></th>
+                            <th style="width:calc(100% / 6)"></th>
+                            <th style="width:calc(100% / 6)"></th>
+                            <th style="width:calc(100% / 6)"></th>
                         </tr>
                     </thead>
                     <tbody>
+                        <tr><th class="h5 font-weight-bold" colspan="6">Kısıtlar</th></tr>    
                         <tr>
-                            <td class="p-1 m-0" colspan="2">
+                            <td class="p-1 m-0" colspan="6">
                                 <div class="form-group p-0 m-0">
                                     <label for="musteri_ara">Müşteri</label>
                                     <input id="musteri_ara" type="text" class="form-control" placeholder="Müşteri Adı">
@@ -162,13 +270,13 @@ echo '<div class="content-wrapper">
                             </td>
                         </tr>
                         <tr>
-                            <td class="p-1 m-0">
+                            <td class="p-1 m-0" colspan="3">
                                 <div class="form-group p-0 m-0">
                                     <label for="cihaz_marka_ara">Cihaz Markası</label>
                                     <input id="cihaz_marka_ara" type="text" class="form-control" placeholder="Cihaz Markası">
                                 </div>
                             </td>
-                            <td class="p-1 m-0">
+                            <td class="p-1 m-0" colspan="3">
                                 <div class="form-group p-0 m-0">
                                     <label for="cihaz_model_ara">Modeli</label>
                                     <input id="cihaz_model_ara" type="text" class="form-control" placeholder="Cihaz Modeli">
@@ -177,7 +285,7 @@ echo '<div class="content-wrapper">
                         </tr>';
 $cihazTurleri = $this->Cihazlar_Model->cihazTurleri();
 echo '<tr>
-                        <td class="p-1 m-0" colspan="2">
+                        <td class="p-1 m-0" colspan="6">
                             <div class="form-group p-0 m-0">
                                 <label for="cihaz_turu">Cihaz Türü</label>
                                 <select id="cihaz_turu" class="form-control" aria-label="Cihaz Türü">
@@ -190,7 +298,7 @@ echo '</select>
                         </td>
                         </tr>';
 echo '<tr>
-                            <td class="p-1 m-0" colspan="2">
+                            <td class="p-1 m-0" colspan="6">
                                 <div class="form-group p-0 m-0">
                                     <label for="yil">Yıl</label>
                                     <input id="yil" type="number" min="1900" max="2099" step="1" class="form-control" placeholder="' . date("Y") . '">
@@ -198,13 +306,13 @@ echo '<tr>
                             </td>
                         </tr>
                         <tr>
-                            <td class="p-1 m-0">
+                            <td class="p-1 m-0" colspan="3">
                                 <div class="form-group p-0 m-0">
                                     <label for="giris_tarihi_baslangic_ara">Giriş Tarihi Başlangıç</label>
                                     <input id="giris_tarihi_baslangic_ara" type="text" class="form-control" placeholder="Giriş Tarihi Başlangıç">
                                 </div>
                             </td>
-                            <td class="p-1 m-0">
+                            <td class="p-1 m-0" colspan="3">
                                 <div class="form-group p-0 m-0">
                                     <label for="giris_tarihi_bitis_ara">Bitiş</label>
                                     <input id="giris_tarihi_bitis_ara" type="text" class="form-control" placeholder="Giriş Tarihi Bitiş">
@@ -213,22 +321,69 @@ echo '<tr>
                         </tr>
                     </tbody>
                 </table>
-                <table id="rapor_tablosu" class="table">
-                    <thead>
-                        <tr>
-                            <th style="width:10% !important;">No</th>
-                            <th style="width:10% !important;">Müşteri</th>
-                            <th style="width:10% !important;">Marka</th>
-                            <th style="width:10% !important;">Model</th>
-                            <th style="width:10% !important;">Tür</th>
-                            <th style="width:10% !important;">Giriş</th>
-                            <th style="width:10% !important;">Çıkış</th>
-                            <th style="width:10% !important;">Sorumlu</th>
-                            <th style="width:10% !important;">Durum</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        ';
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+            <table class="table table-borderless mb-3">
+            <thead>
+                <tr>
+                    <th style="width:calc(100% / 6)"></th>
+                    <th style="width:calc(100% / 6)"></th>
+                    <th style="width:calc(100% / 6)"></th>
+                    <th style="width:calc(100% / 6)"></th>
+                    <th style="width:calc(100% / 6)"></th>
+                    <th style="width:calc(100% / 6)"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr><th class="h5 font-weight-bold" colspan="6">Sayfa Özellikleri</th></tr>
+                <tr>
+                    <td class="p-1 m-0" colspan="2">
+                        <div class="form-group form-check mt-2">
+                            <input type="checkbox" class="form-check-input" id="sayfa_sayisi_ust">
+                            <label class="form-check-label" for="sayfa_sayisi_ust">Üstte Sayfa Sayısını Göster</label>
+                        </div>
+                    </td>
+                    <td class="p-1 m-0" colspan="2"> 
+                        <div class="form-group form-check mt-2">
+                            <input type="checkbox" class="form-check-input" id="sayfa_sayisi_alt">
+                            <label class="form-check-label" for="sayfa_sayisi_alt">Altta Sayfa Sayısını Altta Göster</label>
+                        </div>
+                    </td>
+                    <td class="p-1 m-0" colspan="2">
+                        <div class="form-group">
+                            <label for="sayfaSayisiKonumu">Sayfa Sayısı Konumu</label>
+                            <select class="form-control" id="sayfaSayisiKonumu">
+                                <option value="1">Sağ</option>
+                                <option value="2">Orta</option>
+                                <option value="3">Sol</option>
+                            </select>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+            <table id="rapor_tablosu" class="table">
+            <thead>
+                <tr>
+                    <th style="width:10% !important;">No</th>
+                    <th style="width:10% !important;">Müşteri</th>
+                    <th style="width:10% !important;">Marka</th>
+                    <th style="width:10% !important;">Model</th>
+                    <th style="width:10% !important;">Tür</th>
+                    <th style="width:10% !important;">Giriş</th>
+                    <th style="width:10% !important;">Çıkış</th>
+                    <th style="width:10% !important;">Sorumlu</th>
+                    <th style="width:10% !important;">Durum</th>
+                </tr>
+            </thead>
+            <tbody>
+                ';
 $cihazlar = $this->Cihazlar_Model->cihazlar();
 foreach ($cihazlar as $cihaz) {
     echo '<tr>';
@@ -245,7 +400,7 @@ foreach ($cihazlar as $cihaz) {
 }
 
 echo '                  </tbody>
-                </table>
+        </table>
             </div>
         </div>
     </section>
