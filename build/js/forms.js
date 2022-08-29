4e321676e5e701e05dac17b5728f0ca3d64fb1a8
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-29 16:08:02 +0300

    Cihaz düzenlemede enter ile form onaylama devredışı bırakıldı.

---
+++ build/js/forms.js
@@ -0,0 +1,7 @@
+$("#cihazDuzenleForm, #yapilanIslemlerForm, #upload_form").on("keyup keypress", function (e) {
+	var keyCode = e.keyCode || e.which;
+	if (keyCode === 13) {
+		e.preventDefault();
+		return false;
+	}
+});
