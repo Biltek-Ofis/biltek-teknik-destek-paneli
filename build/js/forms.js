6fc37bc685141df0b80f2084385b87dbcb2ca11c
Ozay Akcan <mail@ozayakcan.com.tr>
2022-08-30 12:33:35 +0300

    Cihaz sayfasındaki formun enter ile onaylanması engelindeki hata giderildi.

--- build/js/forms.js
+++
@@ -1,7 +0,0 @@
-$("#cihazDuzenleForm, #yapilanIslemlerForm, #upload_form").on("keyup keypress", function (e) {
-	var keyCode = e.keyCode || e.which;
-	if (keyCode === 13) {
-		e.preventDefault();
-		return false;
-	}
-});
