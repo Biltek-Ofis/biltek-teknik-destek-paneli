790b35e2c08132eaad5d1ce4242333e9f26b2639
Ozay Akcan <mail@ozayakcan.com.tr>
2022-09-18 02:08:37 +0300

    Responsive menü tasarımı kaldırıldı.

---
+++ lib/sayfalar/sayfa.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../ozellikler/sp.dart';
+import '../widget/menuler.dart';
+import 'giris.dart';
+
+class Sayfa extends StatelessWidget {
+  const Sayfa({
+    super.key,
+    required this.menu,
+    required this.icerik,
+    this.menuGenisligi = 240,
+    this.baslik = "",
+  });
+
+  final AnaMenu menu;
+  final Widget icerik;
+  final double menuGenisligi;
+  final String baslik;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          baslik,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              SharedPref.girisDurumuSil().then((value) {
+                if (value) {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const GirisYap(),
+                    ),
+                    (route) => false,
+                  );
+                }
+              });
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: icerik,
+        ),
+      ),
+      drawer: SizedBox(
+        width: menuGenisligi,
+        child: Drawer(
+          child: menu,
+        ),
+      ),
+    );
+  }
+}
