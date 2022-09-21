ee49d0fb214e5426b206e366452fa34541c544ee
Ozay Akcan <mail@ozayakcan.com.tr>
2022-09-21 22:37:45 +0300

    Uygulamaya token kontrolü eklendi.

--- lib/veritabani/konumlar.dart
+++ lib/veritabani/konumlar.dart
@@ -8,14 +8,14 @@ class Konumlar {
   }
 
   static String girisYap(String kullaniciAdi, String sifre) {
-    return "${getRelativePath}girisyap/$kullaniciAdi/$sifre";
+    return "${getRelativePath}girisyap/$kullaniciAdi/$sifre/${Env.authToken}";
   }
 
   static String kullaniciBilgileri(int id) {
-    return "${getRelativePath}kullaniciBilgileri/$id";
+    return "${getRelativePath}kullaniciBilgileri/$id/${Env.authToken}";
   }
 
   static String cihazlar() {
-    return "${getRelativePath}cihazlarTumu";
+    return "${getRelativePath}cihazlarTumu/${Env.authToken}";
   }
 }
--- php/application/controllers/App.php
+++ php/application/controllers/App.php
@@ -9,29 +9,73 @@ class App extends CI_Controller
         $this->load->model("Kullanicilar_Model");
         $this->load->model("Cihazlar_Model");
     }
-    public function index(){
+    public function index()
+    {
         redirect(base_url());
     }
-    public function girisyap($kullaniciAdi, $sifre){
-        $durum = $this->Giris_Model->girisDurumu($kullaniciAdi, $sifre);
-        $sonuc = array(
-            "id"=> "0",
-            "durum"=> "false"
-        );
-        if($durum){
-            $kullaniciBilgileri = $this->Kullanicilar_Model->tekKullaniciAdi($kullaniciAdi);
-            if(isset($kullaniciBilgileri)){
-                $sonuc["durum"] = "true";
-                $sonuc["id"] = $kullaniciBilgileri->id;
+    public function token($token)
+    {
+        if (isset($token)) {
+            if ($token == getenv("AUTH_TOKEN")) {
+                return true;
+            } else {
+                return false;
             }
+        } else {
+            return false;
         }
-        echo json_encode($sonuc);
     }
-    public function kullaniciBilgileri($id){
-        $kullanici = $this->Kullanicilar_Model->tekKullanici($id);
-        echo json_encode($kullanici);
+    public function hataMesaji($kod)
+    {
+        $sonuc = array(
+            "hata" => $kod
+        );
+        switch ($kod) {
+            case 1:
+                $sonuc["mesaj"] = "Yetkisiz İşlem";
+                break;
+            default:
+                $sonuc["kod"] = 99;
+                $sonuc["mesaj"] = "Bir hata oluştu";
+                break;
+        }
+        return $sonuc;
     }
-    public function cihazlarTumu(){
-        echo json_encode($this->Cihazlar_Model->cihazlarTumuJQ());
+    public function girisyap($kullaniciAdi, $sifre, $token)
+    {
+        if ($this->token($token)) {
+            $durum = $this->Giris_Model->girisDurumu($kullaniciAdi, $sifre);
+            $sonuc = array(
+                "id" => "0",
+                "durum" => "false"
+            );
+            if ($durum) {
+                $kullaniciBilgileri = $this->Kullanicilar_Model->tekKullaniciAdi($kullaniciAdi);
+                if (isset($kullaniciBilgileri)) {
+                    $sonuc["durum"] = "true";
+                    $sonuc["id"] = $kullaniciBilgileri->id;
+                }
+            }
+            echo json_encode($sonuc);
+        } else {
+            echo json_encode($this->hataMesaji(1));
+        }
+    }
+    public function kullaniciBilgileri($id, $token)
+    {
+        if ($this->token($token)) {
+            $kullanici = $this->Kullanicilar_Model->tekKullanici($id);
+            echo json_encode($kullanici);
+        } else {
+            echo json_encode($this->hataMesaji(1));
+        }
+    }
+    public function cihazlarTumu($token)
+    {
+        if ($this->token($token)) {
+            echo json_encode($this->Cihazlar_Model->cihazlarTumuJQ());
+        } else {
+            echo json_encode($this->hataMesaji(1));
+        }
     }
 }
