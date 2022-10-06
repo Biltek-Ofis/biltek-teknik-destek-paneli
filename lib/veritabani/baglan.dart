adc1f8a1346d3ca5bc8f7060e866c29da0ffc1f5
Ozay Akcan <mail@ozayakcan.com.tr>
2022-10-06 19:39:41 +0300

    Bağlantı artık get yerine post ile yapılıyor.

--- lib/veritabani/baglan.dart
+++ lib/veritabani/baglan.dart
@@ -1,20 +1,43 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:http/http.dart' show get;
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../env.dart';
 
 class Baglan {
-  static Future<String> response({required String url}) async {
-    final response = await get(
+  static Future<String> response({
+    required String url,
+    Map<String, String>? postVerileri,
+  }) async {
+    Map<String, String> tokenMap = {
+      "token": Env.authToken,
+    };
+    if (postVerileri == null) {
+      postVerileri = tokenMap;
+    } else {
+      postVerileri.addAll(tokenMap);
+    }
+    final headers = {
+      HttpHeaders.accessControlAllowOriginHeader: "*",
+      HttpHeaders.accessControlAllowCredentialsHeader: "true",
+      HttpHeaders.accessControlAllowMethodsHeader:
+          "GET, POST, OPTIONS, DELETE, PUT",
+      HttpHeaders.accessControlMaxAgeHeader: "86400",
+    };
+    var body = json.encode(postVerileri);
+    if (kDebugMode) {
+      print("Body: $body");
+    }
+    var response = await http.post(
       Uri.parse(url),
-      headers: {
-        HttpHeaders.accessControlAllowOriginHeader: "*",
-        HttpHeaders.accessControlAllowCredentialsHeader: "true",
-        HttpHeaders.accessControlAllowMethodsHeader:
-            "GET, POST, OPTIONS, DELETE, PUT",
-        HttpHeaders.accessControlMaxAgeHeader: "86400",
-      },
+      headers: headers,
+      body: postVerileri,
     );
+    if (kDebugMode) {
+      print(response.body);
+    }
     if (response.statusCode == 200) {
       return response.body;
     } else {
@@ -22,11 +45,23 @@ class Baglan {
     }
   }
 
-  static Future<Map<String, dynamic>> map({required String url}) async {
-    return json.decode(await response(url: url));
+  static Future<Map<String, dynamic>> map({
+    required String url,
+    Map<String, String>? postVerileri,
+  }) async {
+    return json.decode(await response(
+      url: url,
+      postVerileri: postVerileri,
+    ));
   }
 
-  static Future<List> list({required String url}) async {
-    return json.decode(await response(url: url));
+  static Future<List> list({
+    required String url,
+    Map<String, String>? postVerileri,
+  }) async {
+    return json.decode(await response(
+      url: url,
+      postVerileri: postVerileri,
+    ));
   }
 }
