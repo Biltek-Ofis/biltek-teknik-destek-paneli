210f389d913b1db2ce963fa8160ff9df08d35cb3
Ozay Akcan <mail@ozayakcan.com.tr>
2022-12-20 10:44:58 +0300

    Şirket telefonu ayarları eklendi. Birkaç düzenleme yapıldı.

--- application/views/icerikler/yonetim/ayarlar.php
+++ application/views/icerikler/yonetim/ayarlar.php
@@ -1,5 +1,21 @@
 <?php
-
+echo '<script src="https://rawgit.com/RobinHerbots/jquery.inputmask/3.x/dist/jquery.inputmask.bundle.js"></script>';
+echo '<script>
+    function parantezEkle(deger){
+        if(deger.length > 0){
+            $("#baslik_parantez").html("(" + deger + ") ");
+        }else{
+            $("#baslik_parantez").html("");
+        }
+    }
+    $(document).ready(function(){
+        $("#db_telefon").inputmask("(999) 999 99 99");
+        parantezEkle($("#db_baslik").val());
+        $("#db_baslik").keyup(function(){
+            parantezEkle($("#db_baslik").val());
+        });
+    });
+    </script>';
 echo '<div class="content-wrapper">
     <section class="content-header">
         <div class="container-fluid">
@@ -21,16 +37,20 @@ $db_host = explode(",", getenv("DB_HOST"));
 echo '<section class="content">
         <div class="card">
             <div class="card-body">
-                <form method="post" action="'.base_url("yonetim/env_duzenle").'">
+                <form method="post" action="' . base_url("yonetim/env_duzenle") . '">
                     <h4>Genel Ayarlar</h4>
                     <hr>
                     <div class="form-group col">
-                        <label for="db_baslik">Site Başlığı</label>
+                        <label for="db_baslik">Şirket Adı (Kısa)</label>
                         <input id="db_baslik" name="db_baslik" autocomplete="off" class="form-control" type="text" placeholder="Site Başlığı" value="' . getenv("SITE_BASLIGI") . '" required>
                     </div>
                     <div class="form-group col">
                         <label for="db_anasayfa">Şirketinizin Websitesi</label>
                         <input id="db_anasayfa" name="db_anasayfa" autocomplete="off" class="form-control" type="text" placeholder="Şirketinizin Websitesi" value="' . getenv("FIRMA_SITE_URL") . '" required>
+                    </div>
+                    <div class="form-group col">
+                        <label for="db_telefon">Şirket Telefonu</label>
+                        <input id="db_telefon" name="db_telefon" autocomplete="off" class="form-control" type="text" placeholder="Şirket Telefonu" value="' . getenv("SIRKET_TELEFONU") . '" required>
                     </div>
                     <div class="form-group col">
                         <label for="db_anasayfa">Tablolarda Sayfa Başına Gösterilecek Öğe Sayısı</label>
@@ -84,7 +104,7 @@ echo '<section class="content">
                         </div>
                     </div>
                     <div class="form-group col">
-                        <label for="db_barkod_sirket_adi_boyutu">Şirket Adı (Biltek Bilgisayar) ve Tarih Boyutu</label>
+                        <label for="db_barkod_sirket_adi_boyutu">Şirket Adı <span id="baslik_parantez"></span>ve Tarih Boyutu</label>
                         <div class="input-group mb-2">
                             <input id="db_barkod_sirket_adi_boyutu" name="db_barkod_sirket_adi_boyutu" autocomplete="off" class="form-control" type="number" placeholder="Müşteri Adı Boyutu" value="' . getenv("BARKOD_SIRKET_ADI_BOYUTU") . '" required>
                             <div class="input-group-append">
