7cf20c388710963706212f1e45be182e8928da7a
Ozay <mail@ozayakcan.com.tr>
2024-12-22 00:05:55 +0300

    Ayarlar eklendi.

---
+++ app/lib/ayarlar.dart
@@ -0,0 +1,15 @@
+class Ayarlar {
+  static String get _url => const String.fromEnvironment('API_URL');
+
+  static String get url {
+    if (_url.endsWith("/")) {
+      return _url;
+    } else {
+      return "$_url/";
+    }
+  }
+
+  static String get girisYap => "${url}girisyap/";
+  static String get kullaniciGetir => "${url}kullaniciGetir/";
+  static String get token => const String.fromEnvironment('TOKEN');
+}
