d129c1ecc37d56781f09cfdf2e60498f2237a488
Ozay <mail@ozayakcan.com.tr>
2024-12-22 17:00:27 +0300

    Sayfalar duzenlendi.

--- app/lib/sayfalar/anasayfa.dart
+++ app/lib/sayfalar/anasayfa.dart
@@ -2,31 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:teknikservis/sayfalar/cihazlar.dart';
 
 import '../models/kullanici.dart';
-import '../widgets/scaffold.dart';
 
-class Anasayfa extends StatefulWidget {
-  const Anasayfa({super.key, required this.kullanici});
-
+class Anasayfa extends StatelessWidget {
+  const Anasayfa({
+    super.key,
+    required this.kullanici,
+  });
   final KullaniciModel kullanici;
-
-  @override
-  State<Anasayfa> createState() => _AnasayfaState();
-}
-
-class _AnasayfaState extends State<Anasayfa> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: biltekDrawer(
-        context,
-        kullanici: widget.kullanici,
-        seciliSayfa: "Anasayfa",
-      ),
-      appBar: biltekAppBar(
-        context,
-        title: widget.kullanici.adSoyad,
-      ),
-      body: CihazlarSayfasi(),
+    return CihazlarSayfasi(
+      kullanici: kullanici,
+      seciliSayfa: "Anasayfa",
     );
   }
 }
--- app/lib/sayfalar/cihazlarim.dart
+++ app/lib/sayfalar/cihazlarim.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
 
 import '../models/kullanici.dart';
-import '../widgets/scaffold.dart';
 import 'cihazlar.dart';
 
-class CihazlarimSayfasi extends StatefulWidget {
-  const CihazlarimSayfasi({super.key, required this.kullanici});
+class CihazlarimSayfasi extends StatelessWidget {
+  const CihazlarimSayfasi({
+    super.key,
+    required this.kullanici,
+  });
 
   final KullaniciModel kullanici;
-
-  @override
-  State<CihazlarimSayfasi> createState() => _CihazlarimSayfasiState();
-}
-
-class _CihazlarimSayfasiState extends State<CihazlarimSayfasi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: biltekDrawer(
-        context,
-        kullanici: widget.kullanici,
-        seciliSayfa: "Cihazlarım",
-      ),
-      appBar: biltekAppBar(
-        context,
-        title: widget.kullanici.adSoyad,
-      ),
-      body: CihazlarSayfasi(
-        sorumlu: widget.kullanici.id,
-      ),
+    return CihazlarSayfasi(
+      kullanici: kullanici,
+      seciliSayfa: "Cihazlarım",
+      sorumlu: kullanici.id,
     );
   }
 }
