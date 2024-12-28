76b7b066fa31a746fffaf08b9b78ccfe22411483
Ozay Akcan <mail@ozayakcan.com.tr>
2024-12-28 15:05:20 +0300

    Navbar duzenlendi.

--- application/views/inc/navbar.php
+++ application/views/inc/navbar.php
@@ -47,18 +47,21 @@ $detect = new Mobile_Detect();
 if ($detect->isMobile() || $detect->isTablet() || $detect->isAndroidOS()) {
   echo '<div class="w-100 bg-success p-2 text-center"><a href="'.base_url("app/download").'" target="_blank" style="color: blue !important;">Mobil Uygulamayı İndir</a></div>';
 }
-echo '<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-  <ul class="navbar-nav">
-    <!-- <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-    </li>-->
-    <li class="nav-item d-none d-sm-inline-block';
+echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item';
 if ($aktifSayfa == "cihazyonetimi" || $aktifSayfa == "anasayfa") {
     echo " active";
 }
 echo '">
-      <a href="' . base_url() . '" class="nav-link">Anasayfa</a>
-    </li>';
+        <a class="nav-link" href="#">Home</a>
+      </li>';
 if($kullanicibilgileri123["teknikservis"] == 1){
     echo '<li class="nav-item d-none d-sm-inline-block';
 if ($aktifSayfa == "cihazlarim") {
@@ -165,44 +168,10 @@ echo '<li class="nav-item dropdown">
                 </ul>
             </li>';
 echo '
-  </ul>
-  <ul class="navbar-nav ml-auto">
-    <!--<li class="nav-item">
-      <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-        <i class="fas fa-search"></i>
-      </a>
-      <div class="navbar-search-block">
-        <form class="form-inline">
-          <div class="input-group input-group-sm">
-            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-              <button class="btn btn-navbar" type="submit">
-                <i class="fas fa-search"></i>
-              </button>
-              <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                <i class="fas fa-times"></i>
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link" data-toggle="dropdown" href="#">
-        <i class="far fa-bell"></i>
-        <span class="badge badge-warning navbar-badge">15</span>
-      </a>
-      <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <span class="dropdown-item dropdown-header">3 Bildirim</span>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item">
-          <i class="fas fa-file mr-2"></i> 3 yeni cihaz
-          <span class="float-right text-muted text-sm">1 gün</span>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item dropdown-footer">Tüm Bildirimleri Gör</a>
-      </div>
-    </li>-->
+    </ul>
+  </div>
+    <ul class="navbar-nav ml-auto">
+    
     <li class="nav-item">
       <a href="' . base_url("kullanici") . '" class="nav-link" style="color:black;">' . $kullanicibilgileri123["ad_soyad"] . '</a>
     </li>
@@ -218,3 +187,4 @@ echo '
     </li>
   </ul>
 </nav>';
+
