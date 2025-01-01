2de6a52a84ae4fa08a3967cbe0fef3d934731f73
Ozay Akcan <mail@ozayakcan.com.tr>
2025-01-01 17:39:15 +0300

    Yukleniyor alerti eklendi

--- app/lib/utils/alerts.dart
+++ app/lib/utils/alerts.dart
@@ -49,3 +49,25 @@ class Alerts {
     );
   }
 }
+
+void yukleniyor(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      return AlertDialog(
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SizedBox(
+              width: 40,
+              height: 40,
+              child: CircularProgressIndicator(),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
