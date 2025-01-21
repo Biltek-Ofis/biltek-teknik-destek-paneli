5e833040fb181436cda0db34ba31a556b2755a72
Ozay Akcan <mail@ozayakcan.com.tr>
2025-01-21 15:20:09 +0300

    Fazla yer kaplayan eklentiler kaldirildi

--- web/application/views/inc/styles.php
+++ web/application/views/inc/styles.php
@@ -1,6 +1,6 @@
 <?php
 echo '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-<link rel="stylesheet" href="' . base_url("plugins/fontawesome-free/css/all.min.css") . '">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 <link rel="stylesheet" href="' . base_url("dist/css/panel.min.css") . '">
 <link rel="stylesheet" href="' . base_url("dist/css/style.css") . '">';
 
