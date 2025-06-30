473622bf00b6b20644291d02e06c0a10c2d121fe
Ozay <mail@ozayakcan.com.tr>
2025-06-30 11:59:14 +0300

    Galeri bosken resim eklenememesi duzeltildi

--- flutter/lib/sayfalar/detaylar/galery.dart
+++ flutter/lib/sayfalar/detaylar/galery.dart
@@ -62,107 +62,87 @@ class _DetaylarGaleriState extends State<DetaylarGaleri> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${widget.servisNo} Galeri"),
-      ),
+      appBar: AppBar(title: Text("${widget.servisNo} Galeri")),
       bottomNavigationBar: BiltekBottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.add,
-              ),
-              label: "Ekle",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.refresh,
-              ),
-              label: "Resimleri Yenile",
-            ),
-            if (medyalar.isNotEmpty)
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.delete,
-                ),
-                label: "Sil",
-              ),
-          ],
-          currentIndex: 0,
-          selectedItemColor: Theme.of(context).appBarTheme.iconTheme?.color,
-          onTap: (index) async {
-            switch (index) {
-              case 0:
-                if (widget.duzenle && medyalar.isNotEmpty) {
-                  await _medyaYukle();
-                } else {
-                  islemYapilamazDialog(
-                    title: "Eklenemez",
-                    content:
-                        "Cihaz düzenleme tamamlandığı için resim eklenemez!",
-                  );
-                }
-                break;
-              case 1:
-                await _medyalariYenile();
-                break;
-              case 2:
-                if (widget.duzenle) {
-                  _medyaSilDialog();
-                } else {
-                  islemYapilamazDialog(
-                    title: "Silinemez",
-                    content:
-                        "Cihaz düzenleme tamamlandığı için resimler silinemez!",
-                  );
-                }
-                break;
-            }
-            if (widget.duzenle) {}
-          }),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Ekle"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.refresh),
+            label: "Resimleri Yenile",
+          ),
+          if (medyalar.isNotEmpty)
+            BottomNavigationBarItem(icon: Icon(Icons.delete), label: "Sil"),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Theme.of(context).appBarTheme.iconTheme?.color,
+        onTap: (index) async {
+          switch (index) {
+            case 0:
+              if (widget.duzenle) {
+                await _medyaYukle();
+              } else {
+                islemYapilamazDialog(
+                  title: "Eklenemez",
+                  content: "Cihaz düzenleme tamamlandığı için resim eklenemez!",
+                );
+              }
+              break;
+            case 1:
+              await _medyalariYenile();
+              break;
+            case 2:
+              if (widget.duzenle) {
+                _medyaSilDialog();
+              } else {
+                islemYapilamazDialog(
+                  title: "Silinemez",
+                  content:
+                      "Cihaz düzenleme tamamlandığı için resimler silinemez!",
+                );
+              }
+              break;
+          }
+          if (widget.duzenle) {}
+        },
+      ),
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
-        child: yukleniyor
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : (medyalar.isNotEmpty
-                ? PhotoViewGallery.builder(
-                    itemCount: medyalar.length,
-                    builder: (context, index) {
-                      return PhotoViewGalleryPageOptions(
-                        imageProvider: NetworkImage(
-                          medyalar[index].yerel
-                              ? "${Ayarlar.url}${medyalar[index].konum}"
-                              : medyalar[index].konum,
-                        ),
-                        minScale: PhotoViewComputedScale.contained * 1,
-                        maxScale: PhotoViewComputedScale.contained * 4,
-                        onTapDown: (context, details, value) {},
-                      );
-                    },
-                    scrollDirection: Axis.horizontal,
-                    pageController: pageController,
-                    onPageChanged: (index) {
-                      setState(() {
-                        suankiResimIndex = index;
-                      });
-                    },
-                    scrollPhysics: const BouncingScrollPhysics(),
-                    backgroundDecoration: BoxDecoration(
-                      color: Theme.of(context).canvasColor,
-                    ),
-                  )
-                : Center(
-                    child: Text("Henüz resim yüklenmemiş"),
-                  )),
+        child:
+            yukleniyor
+                ? Center(child: CircularProgressIndicator())
+                : (medyalar.isNotEmpty
+                    ? PhotoViewGallery.builder(
+                      itemCount: medyalar.length,
+                      builder: (context, index) {
+                        return PhotoViewGalleryPageOptions(
+                          imageProvider: NetworkImage(
+                            medyalar[index].yerel
+                                ? "${Ayarlar.url}${medyalar[index].konum}"
+                                : medyalar[index].konum,
+                          ),
+                          minScale: PhotoViewComputedScale.contained * 1,
+                          maxScale: PhotoViewComputedScale.contained * 4,
+                          onTapDown: (context, details, value) {},
+                        );
+                      },
+                      scrollDirection: Axis.horizontal,
+                      pageController: pageController,
+                      onPageChanged: (index) {
+                        setState(() {
+                          suankiResimIndex = index;
+                        });
+                      },
+                      scrollPhysics: const BouncingScrollPhysics(),
+                      backgroundDecoration: BoxDecoration(
+                        color: Theme.of(context).canvasColor,
+                      ),
+                    )
+                    : Center(child: Text("Henüz resim yüklenmemiş"))),
       ),
     );
   }
 
-  void islemYapilamazDialog({
-    required String title,
-    required String content,
-  }) {
+  void islemYapilamazDialog({required String title, required String content}) {
     showDialog(
       context: context,
       barrierDismissible: true,
@@ -191,8 +171,9 @@ class _DetaylarGaleriState extends State<DetaylarGaleri> {
         yukleniyor = true;
       });
     }
-    List<MedyaModel> medyalarTemp =
-        await BiltekPost.medyalariGetir(id: widget.id);
+    List<MedyaModel> medyalarTemp = await BiltekPost.medyalariGetir(
+      id: widget.id,
+    );
     if (mounted) {
       setState(() {
         medyalar = medyalarTemp.where((m) => m.tur == "resim").toList();
@@ -221,11 +202,13 @@ class _DetaylarGaleriState extends State<DetaylarGaleri> {
   }
 
   Future<void> _medyaYukle() async {
-    ResimSecici.of(context).sec(resimSecildi: (resim) {
-      if (resim != null) {
-        _resimDuzenle(resim);
-      }
-    });
+    ResimSecici.of(context).sec(
+      resimSecildi: (resim) {
+        if (resim != null) {
+          _resimDuzenle(resim);
+        }
+      },
+    );
 
     //await _medyalariYenile();
   }
@@ -258,44 +241,46 @@ class _DetaylarGaleriState extends State<DetaylarGaleri> {
     Uint8List bytes = await resim.readAsBytes();
     navigatorState.push(
       MaterialPageRoute(
-        builder: (context) => ResimDuzenle(
-          resim: bytes,
-          onEditComplete: (bytes) async {
-            NavigatorState navigatorState = Navigator.of(context);
-            _yukleniyorDialog();
+        builder:
+            (context) => ResimDuzenle(
+              resim: bytes,
+              onEditComplete: (bytes) async {
+                NavigatorState navigatorState = Navigator.of(context);
+                _yukleniyorDialog();
 
-            bool resimYuklendi = await BiltekPost.medyaYukle(
-              id: widget.id,
-              medya: bytes,
-            );
-            navigatorState.pop();
-            if (resimYuklendi) {
-              navigatorState.pop();
-              await _medyalariYenile();
-            } else {
-              if (context.mounted) {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text("Başarısız"),
-                      content: Text(
-                          "Medya yüklenemedi lütfen daha sonra tekrar deneyin."),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text("Kapat"),
-                        ),
-                      ],
-                    );
-                  },
+                bool resimYuklendi = await BiltekPost.medyaYukle(
+                  id: widget.id,
+                  medya: bytes,
                 );
-              }
-            }
-          },
-        ),
+                navigatorState.pop();
+                if (resimYuklendi) {
+                  navigatorState.pop();
+                  await _medyalariYenile();
+                } else {
+                  if (context.mounted) {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Başarısız"),
+                          content: Text(
+                            "Medya yüklenemedi lütfen daha sonra tekrar deneyin.",
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text("Kapat"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                }
+              },
+            ),
       ),
     );
   }
@@ -331,8 +316,9 @@ class _DetaylarGaleriState extends State<DetaylarGaleri> {
     if (medyalar.isNotEmpty) {
       NavigatorState navigatorState = Navigator.of(context);
       _yukleniyorDialog();
-      bool silindi =
-          await BiltekPost.medyaSil(id: medyalar[suankiResimIndex].id);
+      bool silindi = await BiltekPost.medyaSil(
+        id: medyalar[suankiResimIndex].id,
+      );
       navigatorState.pop();
       if (silindi) {
         await _medyalariYenile();
@@ -344,8 +330,9 @@ class _DetaylarGaleriState extends State<DetaylarGaleri> {
             builder: (context) {
               return AlertDialog(
                 title: Text("Başarısız"),
-                content:
-                    Text("Medya silinemedi lütfen daha sonra tekrar deneyin."),
+                content: Text(
+                  "Medya silinemedi lütfen daha sonra tekrar deneyin.",
+                ),
                 actions: [
                   TextButton(
                     onPressed: () {
