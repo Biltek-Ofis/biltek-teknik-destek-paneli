bfada33aa3e9fa13a633b9736b9e55f52ec0ec25
Ozay <mail@ozayakcan.com.tr>
2025-07-03 10:52:47 +0300

    Markdown support ve ikon eklendi

--- flutter/lib/utils/assets.dart
+++ flutter/lib/utils/assets.dart
@@ -1,6 +1,7 @@
 class BiltekAssets {
   static String get _images => "images";
   static String get logo => "$_images/logo.png";
+  static String get icon => "$_images/icon.png";
   static String get whatsapp => "$_images/whatsapp.png";
   static String get _sounds => "sounds";
   static String get barkod => "$_sounds/barkod.mp3";
