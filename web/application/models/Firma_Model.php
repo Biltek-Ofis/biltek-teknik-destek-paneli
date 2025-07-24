21ff8b0ce0ae90dc06574b053cd08a5de7bb7f17
Ozay <mail@ozayakcan.com.tr>
2025-07-24 17:53:22 +0300

    Musteriler artik isme gore siralaniyor

--- web/application/models/Firma_Model.php
+++ web/application/models/Firma_Model.php
@@ -16,7 +16,7 @@ class Firma_Model extends CI_Model
     public $stokTablosu = "TBLSTSABIT";
     public function musteriBilgileri($aranacak, $ara, $limit = 0)
     {
-        return $this->ara($this->musteriTablosu(), $aranacak, $ara, $limit);
+        return $this->ara($this->musteriTablosu(), $aranacak, $ara, $limit, array('musteri_adi' => 'ASC'));
         //return $this->firmadb()->like('LOWER(' . $aranacak . ')', strtolower($ara))->get($this->musteriTablosu())->result();
     }
     public function stok($aranacak, $ara, $limit = 0)
@@ -24,13 +24,18 @@ class Firma_Model extends CI_Model
         return $this->ara($this->stokTablosu, $aranacak, $ara, $limit);
         //return $this->firmadb()->like($aranacak, "N'" . $ara . "'")->get($this->stokTablosu)->result();
     }
-    public function ara($tablo, $aranacak, $ara, $limit = 0)
+    public function ara($tablo, $aranacak, $ara, $limit = 0, $order_by = array())
     {
         //$ara = $this->Islemler_Model->turkceKarakterArama($ara);
         //return $this->firmadb()->query("SELECT * FROM " . $tablo . " WHERE " . $aranacak . " LIKE N'%" . $ara . "%' collate Turkish_CI_AS")->result();
         $query = $this->db->reset_query()->like($aranacak, $ara);
         if($limit > 0){
             $query = $query->limit($limit);
+        }
+        if(count($order_by) > 0) {
+            foreach ($order_by as $column => $direction) {
+                $query = $query->order_by($column, $direction);
+            }
         }
         return $query->get($tablo)->result();
     }
