44897030399ccdea8f9e0ae2bb73da249e37b07c
Ozay Akcan <mail@ozayakcan.com.tr>
2025-10-07 10:14:13 +0300

    Cagri kaydi duzenleme eklendi

---
+++ web/application/views/inc/modal_cagri_kaydi_duzenle.php
@@ -0,0 +1,87 @@
+<div class="modal fade" id="cagriKaydiDuzenleModal" tabindex="-1" role="dialog"
+    aria-labelledby="cagriKaydiDuzenleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="cagriKaydiDuzenleModalTitle">Çağrı Kaydı Duzenle</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="cagriKaydiDuzenleForm" <?= isset($form_action) ? ' action="' . $form_action . '"' : ""; ?>
+                    method="post">
+                    <div class="row">
+                        <h6 class="col">Gerekli alanlar * ile belirtilmiştir.</h6>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/bolge", array(
+                            "bolge_value" => isset($bolge_value) ? $bolge_value : "",
+                        ));
+                        ?>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/birim", array(
+                            "birim_value" => isset($birim_value) ? $birim_value : "",
+                        ));
+                        ?>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/gsm", array(
+                            "telefon_numarasi_label" => TRUE,
+                            "gsm_id" => "telefon_numarasi2",
+                            "telefon_numarasi_value" => isset($telefon_numarasi_value) ? $telefon_numarasi_value : "",
+                        ));
+                        ?>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/cihaz_turleri", array(
+                            "cihaz_turleri_label" => TRUE,
+                            "cihaz_turu_value" => isset($cihaz_turu_value) ? $cihaz_turu_value : "",
+                        ));
+                        ?>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/cihaz_markasi", array(
+                            "cihaz_markasi_label" => TRUE,
+                            "cihaz_value" => isset($cihaz_value) ? $cihaz_value : "",
+                        ));
+                        ?>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/cihaz_modeli", array(
+                            "cihaz_modeli_label" => TRUE,
+                            "cihaz_modeli_value" => isset($cihaz_modeli_value) ? $cihaz_modeli_value : "",
+                        ));
+                        ?>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/seri_no", array(
+                            "seri_no_label" => TRUE,
+                            "seri_no_value" => isset($seri_no_value) ? $seri_no_value : "",
+                        ));
+                        ?>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $this->load->view("ogeler/ariza_aciklamasi", array(
+                            "ariza_aciklamasi_label" => TRUE,
+                            "ariza_aciklamasi_value" => isset($ariza_aciklamasi_value) ? $ariza_aciklamasi_value : "",
+                        ));
+                        ?>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button id="cagriKaydiDuzenleKaydetBtn" class="btn btn-success" type="submit"
+                    form="cagriKaydiDuzenleForm">Kaydet</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">İptal</button>
+            </div>
+        </div>
+    </div>
+</div>
