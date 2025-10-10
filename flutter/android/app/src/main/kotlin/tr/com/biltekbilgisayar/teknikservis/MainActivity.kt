795358f2efa4d43ed8c047655f7548b87d436761
Ozay Akcan <mail@ozayakcan.com.tr>
2025-10-10 16:39:39 +0300

    Biyometrik verilerle giriş eklendi

--- flutter/android/app/src/main/kotlin/tr/com/biltekbilgisayar/teknikservis/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/tr/com/biltekbilgisayar/teknikservis/MainActivity.kt
@@ -3,10 +3,11 @@ package tr.com.biltekbilgisayar.teknikservis
 import android.content.Intent
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity(){
+class MainActivity : FlutterFragmentActivity(){
     private val CHANNEL = "teknikservis/notifications"
     private var methodChannel: MethodChannel? = null
     private var pendingNotificationData: Map<String, String?>? = null
