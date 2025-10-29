027f711d12ebfb07a8f92121e219067c5cfcf287
Ozay Akcan <mail@ozayakcan.com.tr>
2025-10-29 10:42:41 +0300

    Şarj'ın yanlis yazimi otomatik duzeltiliyor

--- web/application/views/inc/scripts.php
+++ web/application/views/inc/scripts.php
@@ -5,5 +5,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 	integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
 	crossorigin="anonymous"></script>
+<script>
+	$(document).ready(function(){
+		$('input, textarea').on("keyup change", function(){
+			var val = $(this).val();
+			var corrected = val.replace(/sarz|şarz/gi, function(match) {
+				return match === match.toUpperCase() ? "ŞARJ" : "şarj";
+			});
+			if (corrected !== val) {
+				$(this).val(corrected);
+			}
+		});
+	});
+</script>
 <?php
 $this->load->view("inc/ortak/scripts");
