9b26c0860b823f40908fff5a383a1aaed5162dd3
Ozay Akcan <mail@ozayakcan.com.tr>
2025-11-18 17:53:40 +0300

    Datatables yerel kullanimi eklendi

--- web/application/views/inc/datatables_scripts.php
+++ web/application/views/inc/datatables_scripts.php
@@ -1,18 +1,18 @@
-<script src="https://cdn.datatables.net/2.3.0/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/2.3.0/js/dataTables.bootstrap5.js"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/dataTables.js") : "https://cdn.datatables.net/2.3.0/js/dataTables.js"; ?>"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/dataTables.bootstrap5.js") : "https://cdn.datatables.net/2.3.0/js/dataTables.bootstrap5.js"; ?>"></script>
 
-<script src="https://cdn.datatables.net/responsive/3.0.4/js/dataTables.responsive.js"></script>
-<script src="https://cdn.datatables.net/responsive/3.0.4/js/responsive.bootstrap5.js"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/dataTables.responsive.js") : "https://cdn.datatables.net/responsive/3.0.4/js/dataTables.responsive.js"; ?>"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/responsive.bootstrap5.js") : "https://cdn.datatables.net/responsive/3.0.4/js/responsive.bootstrap5.js"; ?>"></script>
 
-<script src="https://cdn.datatables.net/datetime/1.5.5/js/dataTables.dateTime.js"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/dataTables.dateTime.js") : "https://cdn.datatables.net/datetime/1.5.5/js/dataTables.dateTime.js"; ?>"></script>
 
 <script src="<?= base_url("plugins/jszip/jszip.min.js") ;?>"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.12/pdfmake.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.12/vfs_fonts.js"></script>
 
-<script src="https://cdn.datatables.net/buttons/3.2.3/js/dataTables.buttons.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.bootstrap5.js"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/dataTables.buttons.js") : "https://cdn.datatables.net/buttons/3.2.3/js/dataTables.buttons.js"; ?>"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/buttons.bootstrap5.js") : "https://cdn.datatables.net/buttons/3.2.3/js/buttons.bootstrap5.js"; ?>"></script>
 
-<script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.html5.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.print.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.colVis.js"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/buttons.html5.js") : "https://cdn.datatables.net/buttons/3.2.3/js/buttons.html5.js"; ?>"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/buttons.print.js") : "https://cdn.datatables.net/buttons/3.2.3/js/buttons.print.js"; ?>"></script>
+<script src="<?= DATATABLES_LOCAL ? base_url("plugins/datatables/js/buttons.colVis.js") : "https://cdn.datatables.net/buttons/3.2.3/js/buttons.colVis.js"; ?>"></script>
