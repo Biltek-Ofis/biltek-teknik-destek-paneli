68356c6e8b06ca8a09e5356c56dad211aa483bd1
Ozay Akcan <mail@ozayakcan.com.tr>
2026-06-10 10:40:05 +0300

    Flutter version guncellemesi

--- flutter/android/app/build.gradle.kts
+++ flutter/android/app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
 
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -34,10 +33,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "tr.com.biltekbilgisayar.teknikservis"
@@ -57,24 +52,32 @@ android {
             keyPassword = keystoreProperties["keyPassword"] as String
         }
     }
+
+    
     buildTypes {
-        getByName("debug") {
+        debug {
             signingConfig = signingConfigs.getByName("debug")
         }
-        getByName("release") {
+        release {
             signingConfig = signingConfigs.getByName("release")
         }
     }
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.14.1"))
     implementation("com.google.firebase:firebase-messaging")
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-    implementation("androidx.window:window:1.0.0")
-    implementation("androidx.window:window-java:1.0.0")
-    implementation("com.google.android.material:material:1.12.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+    implementation("androidx.window:window:1.5.1")
+    implementation("androidx.window:window-java:1.5.1")
+    implementation("com.google.android.material:material:1.14.0")
 }
 
 flutter {
