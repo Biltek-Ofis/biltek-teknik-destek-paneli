a00f897551303a53be56c3b84b6bfa621fd6501e
Ozay Akcan <mail@ozayakcan.com.tr>
2026-06-15 15:19:42 +0300

    Firebase versiyonlari yukseltildi

--- flutter/web/firebase-messaging-sw.js
+++ flutter/web/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // Service Worker
-importScripts('https://www.gstatic.com/firebasejs/12.7.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/12.7.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js');
 importScripts('firebase-variables.js');
 
 const siteDomain = tsSiteUrl;
